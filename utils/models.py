9e7d23bb831baf3f1dea8b2ac0122c423c95fbc9
Alexander <you@example.com>
2021-10-23 22:54:47 +0300

    resnet50 as feature extractor

--- utils/models.py
+++ utils/models.py
@@ -7,13 +7,16 @@ class FeatureExtractor(nn.Module):
         super(FeatureExtractor, self).__init__()
         if network == 'vgg16':
             model = torchvision.models.vgg16(pretrained=True)
+            self.features = model.features
         elif network == 'resnet50':
             model = torchvision.models.resnet50(pretrained=True)
+            model.fc = nn.Identity()
+            self.features = model
         else:
             model = torchvision.models.alexnet(pretrained=True)
+            self.features = list(model.children())[0] 
+            
         model.eval() # to not do dropout
-        self.features = list(model.children())[0] 
-        self.classifier = nn.Sequential(*list(model.classifier.children())[:-2])
     def forward(self, x):
         x = self.features(x)
         return x
